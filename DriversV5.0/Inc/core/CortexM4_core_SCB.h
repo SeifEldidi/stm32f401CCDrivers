583e4773761f4d9ae0de52d477f758a899fb7f47
Seif Hisham Eldidi <106155115+SeifEldidi@users.noreply.github.com>
2023-07-27 20:08:18 +0300

    Add files via upload

---
+++ DriversV5.0/Inc/core/CortexM4_core_SCB.h
@@ -0,0 +1,34 @@
+/*
+ * CortexM4_core_SCB.h
+ *
+ *  Created on: Jul 27, 2023
+ *      Author: Seif pc
+ */
+
+#ifndef CORE_CORTEXM4_CORE_SCB_H_
+#define CORE_CORTEXM4_CORE_SCB_H_
+
+/*-------------Includes Section Start---------------*/
+#include <stdint.h>
+#include "../std_macros.h"
+/*-------------Includes Section End----------------*/
+
+/*------------Macro Declarations Start-----------*/
+
+/*------------Macro Declarations End-----------*/
+
+/*-----------Function Macro Start----------------*/
+
+/*-----------Function Macro End----------------*/
+
+/*----------Defined Data types start-------------------*/
+
+
+/*----------Defined Data types End----------------*/
+
+/*-----------Software Interfaces start--------------*/
+
+/*-----------Software Interfaces End--------------*/
+
+
+#endif /* CORE_CORTEXM4_CORE_SCB_H_ */
